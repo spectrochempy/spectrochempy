8ebf8077a66a96d91f247b8eab61893a87d5f286
Christian Fernandez <christian.fernandez@ensicaen.fr>
2017-11-18 19:20:45 +0100

    FIX: Correction of a bug on saving dataset in scp file format when they were containing mpl axes

--- tests/test_agir_application.py
+++ tests/test_agir_application.py
@@ -44,49 +44,47 @@ def samples():
     _samples = {'P350':{'label':'$\mathrm{M_P}\,(623\,K)$'},
                'A350':{'label':'$\mathrm{M_A}\,(623\,K)$'},
                'B350':{'label':'$\mathrm{M_B}\,(623\,K)$'}}
-    return  _samples
 
-def test_agir_application(samples):
-
-    for key in samples.keys():
-        basename = os.path.join(scpdata,'agirdata/{}/TGA/tg'.format(key))
+    for key, sample in _samples.items():
+        # our data are in our test `scpdata` directory.
+        basename = os.path.join(scpdata,'agirdata/{}/FTIR/FTIR'.format(key))
         if os.path.exists(basename+'.scp'):
-            os.remove(basename + '.scp')
+            #check if the scp file have already been saved
+            filename = basename + '.scp'
+            sample['IR'] = NDDataset.read( filename)
+        else:
+            # else read the original zip file
+            filename = basename + '.zip'
+            sample['IR'] = NDDataset.read_zip( filename, origin='omnic_export')
+            # save
+            sample['IR'].save(basename + '.scp')
 
-        # else read the original csv file
-        filename = basename + '.csv'
-        ss = samples[key]['TGA'] = NDDataset.read_csv(filename)
-        # lets keep only data from somrthing close to 0.
-        s = samples[key]['TGA'] = ss[-0.5:60.0]
-        # for TGA, some information are missing.
-        # we add them here
-        s.x.units = 'hour'
-        s.units = 'weight_percent'
-        s.x.title = 'Time on stream'
-        s.title = 'Mass variation'
+    for key, sample in _samples.items():
+        basename = os.path.join(scpdata, 'agirdata/{}/TGA/tg'.format(key))
+        if os.path.exists(basename + '.scp'):
+            # check if the scp file have already been saved
+            filename = basename + '.scp'
+            sample['TGA'] = NDDataset.read(filename)
+        else:
+            # else read the original csv file
+            filename = basename + '.csv'
+            ss = sample['TGA'] = NDDataset.read_csv(filename)
+            # lets keep only data from something close to 0.
+            s = sample['TGA'] = ss[-0.5:35.0]
+            # for TGA, some information are missing.
+            # we add them here
+            s.x.units = 'hour'
+            s.units = 'weight_percent'
+            s.x.title = 'Time-on-stream'
+            s.title = 'Mass change'
+            # save
+            sample['TGA'].save(basename + '.scp')
 
-        # save
-        samples[key]['TGA'].save(basename + '.scp')
-
-        # load
-        samples[key]['TGA1'] = NDDataset.read(basename + '.scp')
-        assert samples[key]['TGA1'] == samples[key]['TGA']
+    return _samples
 
 
 def test_slicing_agir(samples):
 
-    for key in samples.keys():
-        # our data are in our test `scpdata` directory.
-        basename = os.path.join(scpdata, 'agirdata/{}/FTIR/FTIR'.format(key))
-        filename = basename + '.scp'
-        samples[key]['IR'] = NDDataset.read(filename)
-
-    for key in samples.keys():
-        s = samples[key]['IR']
-        s.plot(kind='stack')
-        label = samples[key]['label']
-        title = 'IR spectra for sample {}'.format(label)
-        s.ax.set_title(title, fontsize=16)
 
     # We will resize the data in the interesting region of wavenumbers
 
