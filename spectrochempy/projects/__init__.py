6134f1d5c5893293a8e2c21a1cf950da5d0f4f4c
Christian Fernandez <christian.fernandez@ensicaen.fr>
2017-12-14 23:17:44 +0100

    LICENSE string changed

--- spectrochempy/projects/__init__.py
+++ spectrochempy/projects/__init__.py
@@ -3,14 +3,7 @@
 # =============================================================================
 # Copyright (©) 2015-2017 LCS
 # Laboratoire Catalyse et Spectrochimie, Caen, France.
-#
-# This software is a computer program whose purpose is to provide a general
-# API for displaying, processing and analysing spectrochemical data.
-#
-# This software is governed by the CeCILL license under French law and
-# abiding by the rules of distribution of free software. You can use,
-# modify and/ or redistribute the software under the terms of the CeCILL
-# license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info".
+# CeCILL FREE SOFTWARE LICENSE AGREEMENT (Version 2.1)
+# See full LICENSE agreement in the root directory
 # =============================================================================
 
