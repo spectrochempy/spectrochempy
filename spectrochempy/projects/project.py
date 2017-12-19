f8a8ae4984c3d0430018b0ed71e47c66d4fce381
Christian Fernandez <christian.fernandez@ensicaen.fr>
2017-12-19 20:40:01 +0100

    FIX: problem when project has been unzipped.

--- spectrochempy/projects/project.py
+++ spectrochempy/projects/project.py
@@ -778,7 +778,8 @@ class Project(AbstractProject):
                           "from filename!", SpectroChemPyWarning)
 
         filename = os.path.expanduser(os.path.join(directory, filename))
-        if not os.path.exists(filename) and not filename.endswith('.pscp'):
+        if (not os.path.exists(filename) or os.path.isdir(filename)) \
+                and not filename.endswith('.pscp'):
             filename = filename + '.pscp'
             if not os.path.exists(filename):
                 raise IOError('no valid project filename provided')
