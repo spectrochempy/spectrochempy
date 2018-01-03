51d71bb8e5b0043475357e2e5c72fda281f97fba
Christian Fernandez <christian.fernandez@ensicaen.fr>
2018-01-03 11:52:29 +0100

    DEV : created a module to generate fake data for testing analysis MCRALS and EFA.

---
+++ spectrochempy/utils/fake.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+# =============================================================================
+# Copyright (©) 2015-2018 LCS
+# Laboratoire Catalyse et Spectrochimie, Caen, France.
+# CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
+# See full LICENSE agreement in the root directory
+# =============================================================================
+
+"""
+This module implements routines to generate fake data that can be used for
+testing our various |scp| analysis methods
+
+"""
+
+import numpy as np
+
+from spectrochempy.core.fitting.models import gaussianmodel
+from spectrochempy.dataset.ndcoords import Coord
+from spectrochempy.dataset.nddataset import NDDataset
+from spectrochempy.core.processors.npy import dot
+from spectrochempy.utils import show
+
+
+def _make_spectra_matrix(pos, width, ampl):
+    x = Coord(np.linspace(6000.0, 1000.0, 4000), units='cm^-1',
+              title='wavenumbers')
+    s = []
+    for args in zip(ampl, width, pos):
+        s.append(gaussianmodel().f(x.data, *args))
+
+    st = np.vstack(s)
+    st = NDDataset(data=st, units='absorbance', title='absorbance',
+                   coordset = [range(len(st)), x])
+
+    return st
+
+def _make_concentrations_matrix(*profiles):
+    t = Coord(np.linspace(0,10,50), units='hour', title='time')
+    c = []
+    for p in profiles:
+        c.append(p(t.data))
+    ct = np.vstack(c)
+    ct = ct-ct.min()
+    ct = ct / np.sum(ct, axis=0)
+    ct = NDDataset(data=ct, title='concentration', coordset=[range(len(ct)),t])
+
+    return ct
+
+def generate_2D_spectra(concentrations, spectra):
+    """
+    Generate a fake 2D experimental spectra
+
+    Parameters
+    ----------
+    concentrations : |NDDataset|
+    spectra : |NDDataset|
+
+    Returns
+    -------
+    |NDDataset|
+
+    """
+    return dot(concentrations.T, spectra)
+
+# define properties of the spectra and concentration profiles
+# ------------------------------------------------------------
+
+POS = (6000., 4000., 2000., 2500.)
+WIDTH = (6000., 1000., 600., 800.)
+AMPL = (100., 100., 20., 50.)
+C1 = lambda t: t * .05 + .01  # linear evolution of the baseline
+C2 = lambda t: np.exp(-t/.5)*.3+.1
+C3 = lambda t: np.exp(-t/3.)*.7
+C4 = lambda t: 1. - C2(t) - C3(t)
+
+spec = _make_spectra_matrix(POS, WIDTH, AMPL)
+spec.plot_stack(colorbar=False)
+
+conc = _make_concentrations_matrix(C1, C2, C3, C4)
+conc.plot_stack(colorbar=False)
+
+d = generate_2D_spectra(conc, spec)
+d.plot_stack()
+
+show()
+
+d.save('test_full2D')
+spec.save('test_spectra')
+conc.save('test_concentration')
+
+# =============================================================================
+if __name__ == '__main__':
+    pass
