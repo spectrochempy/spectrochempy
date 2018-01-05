ac12357f188f9e393bf7959f107f6db128919c07
Christian Fernandez <christian.fernandez@ensicaen.fr>
2018-01-05 19:24:53 +0100

    FIX: requirements.txt and conf.py to generate documentation

--- docs/conf.py
+++ docs/conf.py
@@ -232,7 +232,7 @@ latex_documents = [(
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = "_static/logo.png"
+latex_logo = "_static/scpy.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
