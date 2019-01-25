bfd493b36853eff1300d865a4e80669550f8362c
Arnaud Travert <arnaud.travert@ensicaen.fr>
2019-01-25 23:10:04 +0100

    FIX issue #25

--- spectrochempy/core/readers/readcsv.py
+++ spectrochempy/core/readers/readcsv.py
@@ -25,7 +25,13 @@ import locale
 try:
     locale.setlocale(locale.LC_ALL, 'en_US')  # to avoid problems with date format
 except:
-    warnings.warn('Could not set locale: en_US')
+    try:
+        locale.setlocale(locale.LC_ALL, 'en_US.uft-8')  # idem
+    except:
+        try:
+            locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # idem
+        except:
+            warnings.warn('Could not set locale: en_US')
 
 # ----------------------------------------------------------------------------
 # third party imports
