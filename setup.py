2fed08cf6730ebb983fc97932677d5af817428ec
Christian Fernandez <christian.fernandez@ensicaen.fr>
2020-04-20 23:08:29 +0200

    WIP setup.py

--- setup.py
+++ setup.py
@@ -110,20 +110,21 @@ def read(fname):
 # Data for setuptools
 packages = []
 
-# extras_require = {
-#     'tests': [
-#         "pytest",
-#         "pytest-runner",
-#         "pytest-console-scripts",
-#         "scikit-image",
-#     ],
-# }
+extras_require = {
+    'tests': [
+        "pytest",
+        "pytest-runner",
+        "pytest-console-scripts",
+        "scikit-image",
+    ],
+}
 
 setup_args = dict(
     
     # packages informations
     name="spectrochempy",
-    use_scm_version=True,
+    #use_scm_version=True,
+    version = '1.14dev',
     license="CeCILL-B",
     author="Arnaud Travert & Christian Fernandez",
     author_email="developpers@spectrochempy.fr",
@@ -152,7 +153,7 @@ setup_args = dict(
     
     # requirements
     python_requires=">3.5",  # TODO: check if it works also with 3.5
-    # setup_requires=['setuptools_scm', 'pyyaml'],
+    setup_requires=['setuptools_scm', 'pyyaml'],
     install_requires=[],  # install_requires(dev=__DEV__),
     #tests_require=extras_require['tests'],
     
