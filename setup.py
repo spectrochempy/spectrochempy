17a3533ce727102c4ace615db73801f36a41af47
Christian Fernandez <christian.fernandez@ensicaen.fr>
2020-05-23 23:07:56 +0200

    DEV: setup for automatic upload started

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup_args = dict(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     entry_points={
         'console_scripts': ['scpy=spectrochempy.scripts.launch_api:main', ],
     },
