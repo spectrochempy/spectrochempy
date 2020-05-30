8eb73dcf1689966109657abe8678dfcd8e22044e
atravert <arnaud.travert@ensicaen.fr>
2020-05-31 00:00:39 +0200

    FIX #76 read_opus() shifts the xaxis (redmine issue 75)
    fix a remaining bug and add test_read_opus

---
+++ tests/test_readers/test_read_opus.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# ======================================================================================================================
+# Copyright (©) 2015-2020 LCS
+# Laboratoire Catalyse et Spectrochimie, Caen, France.
+# CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
+# See full LICENSE agreement in the root directory
+# ======================================================================================================================
+
+import os
+from spectrochempy.core.dataset.nddataset import NDDataset
+import pytest
+
+
+def test_read_with_filename():
+    A = NDDataset.read_opus(os.path.join('irdata', 'OPUS', 'test.0000'))
+    assert A[0, 2303.8694].data == pytest.approx(2.72740, 0.00001)
+    B = NDDataset.read_opus(['test.0000', 'test.0001', 'test.0002'],
+                            directory=os.path.join('irdata', 'OPUS', ))
+    assert len(B) == 3
+
+
+
