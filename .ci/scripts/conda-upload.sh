996c8e2aa61d4c5aa734b939404d6a2f3b40838e
Christian Fernandez <>
2020-06-13 12:22:53 +0200

    WIP

--- .ci/scripts/conda-upload.sh
+++ .ci/scripts/conda-upload.sh
@@ -9,6 +9,35 @@ set -ex
 PKG_NAME=spectrochempy
 OS=noarch
 
+#!/usr/bin/env bash
+
+PKG_NAME=spectrochempy
+
+## Exit immediately if a command exits with a non-zero status.
+set -e
+
+## get version string from setuptools_scm
+PVS=$(python setup.py --version)
+ECHO "Current version string = $PVS"
+
+## Extract components
+IFS=$"+"
+read -ra arr <<< "$PVS"
+
+## latest version string
+LATEST="${arr[0]}"
+
+## find the stable tag
+IFS=$"."
+read -ra tag <<< "$LATEST";
+DEVSTRING="${tag[3]}"
+VERSION="${tag[0]}.${tag[1]}.${tag[2]}"
+if [[ $DEVSTRING ]]; then
+  PKG_NAME_VERSION="$PKG_NAME-$VERSION.$DEVSTRING.tar.bz2"
+else
+  PKG_NAME_VERSION="$PKG_NAME-$VERSION.tar.bz2"
+fi
+
 ## Avoid uploading automatically
 conda config --set anaconda_upload no
 
