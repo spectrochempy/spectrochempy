2f7059be4785214329c15438b2b4b75799767d23
Christian Fernandez <>
2020-06-21 20:06:16 +0200

    FIX: check update and the lgtm warnings

--- spectrochempy/scripts/scpy_update.py
+++ spectrochempy/scripts/scpy_update.py
@@ -29,7 +29,8 @@ but version {new_version} is available.
     
 Please consider updating for bug fixes and new features !
 
-Use: conda update {'-c spectrocat/label/dev ' if  'dev' in new_version else ''}spectrochempy"""
+Use: 
+conda update {'-c spectrocat/label/dev ' if  'dev' in new_version else ''}spectrochempy"""
             QMainWindow.__init__(self)
             QMessageBox.warning(self, "SpectroChempy has been updated", message)
 
@@ -58,22 +59,27 @@ Use: conda update {'-c spectrocat/label/dev ' if  'dev' in new_version else ''}s
         new_major, new_minor, new_patch, new_dev = major, minor, patch, dev
         upd = False
         dev_upd=False
+        print(major, minor, patch, dev)
         for k, v in vavailables.items():
             _major, _minor, _patch = list(map(int, v[:3]))
             _dev = int(v[3][3:]) if v[3] is not None and 'dev' in v[3] else 0
+            print (_major, _minor, _patch, _dev)
+            if _major > new_major:
+                new_major = _major
+                new_dev = 0  # reset dev
+                upd = True
+            elif _minor > new_minor:
+                new_minor = _minor
+                new_dev = 0  # reset dev
+                upd = True
+            elif _patch > new_patch:
+                new_patch = _patch
+                new_dev = 0  # reset dev
+                upd = True
 
-            if new_major > major:
-                new_major = major
-                upd = True
-            elif new_minor > minor:
-                new_minor = minor
-                upd = True
-            elif new_patch > patch:
-                new_patch = patch
-                upd = True
-
-            elif new_dev > dev:
-                new_dev = dev
+            # check dev
+            if _dev > new_dev and _major >= new_major and _minor >= new_minor and _patch >= new_patch:
+                new_dev = _dev
                 upd = True
                 dev_upd = True
 
@@ -81,12 +87,12 @@ Use: conda update {'-c spectrocat/label/dev ' if  'dev' in new_version else ''}s
         if upd:
             new_version = f'{new_major}.{new_minor}.{new_patch}'
             if dev_upd:
-                new_version = f'{new_version}-dev{new_dev}'
+                new_version = f'{new_version}.dev{new_dev}'
 
         return new_version
 
 def main():
-    app = QApplication(sys.argv)
+    _ = QApplication(sys.argv)
     UpdateWindow()
     sys.exit(0)
 
