98fc2c61522fc147253b052df93a54e7b022bed4
Christian Fernandez <>
2020-09-28 08:53:07 +0200

    DEV: scpy dash application

--- spectrochempy/scripts/scpy.py
+++ spectrochempy/scripts/scpy.py
@@ -1,31 +1,32 @@
 # -*- coding: utf-8 -*
+# %% [markdown]
+# # SpectroChemPy GUI: Dash Application
 
+# %% [markdown]
+# Copyright (©) 2015-2020 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.                                  
+# CeCILL-B FREE SOFTWARE LICENSE AGREEMENT - See full LICENSE agreement in the root directory                        
 
-# ======================================================================================================================
-#  Copyright (©) 2015-2020 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.                                  =
-#  CeCILL-B FREE SOFTWARE LICENSE AGREEMENT - See full LICENSE agreement in the root directory                         =
-# ======================================================================================================================
-
-"""This module is the main entry-point for the GUI application launched from the
-terminal command line
-
-"""
+# %%
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+# %%
 import datetime
 import base64
 import io
 
+# %%
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 
+# %%
 import spectrochempy as scp
 from spectrochempy.core import app, info_
 
+# %%
 def gui():
 
     # Initialisation ...................................................................................................
@@ -102,6 +103,7 @@ def gui():
 
     dashapp.run_server(debug=True)
 
+# %%
 # ======================================================================================================================
 if __name__ == '__main__':
     gui( )
