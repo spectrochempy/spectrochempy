5c61906960a06bced1a3a7426c3e30431830a47c
Christian Fernandez <christian.fernandez@ensicaen.fr>
2021-01-28 17:20:18 +0100

    FIX: fix the name of an environment

--- .ci/env/env_create.py
+++ .ci/env/env_create.py
@@ -20,7 +20,9 @@ env = Path(__file__).parent
 tempfile = env / "env_template.yml"
 template = Template(tempfile.read_text('utf-8'))
 
-out = template.render(VERSION=args.version, DEV=args.dev, DASH=args.dash, CANTERA=args.cantera)
+name = args.name.split(".")[0]
+
+out = template.render(NAME=name, VERSION=args.version, DEV=args.dev, DASH=args.dash, CANTERA=args.cantera)
 
 filename = (env / args.name).with_suffix('.yml')
 filename.write_text(out)
