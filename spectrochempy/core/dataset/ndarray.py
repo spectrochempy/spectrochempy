FIX: issue #255 + add an example with units
The method to() has been rewritten as it was incorrect

spectrochempy/core/dataset/ndarray.py
@@ -2265,6 +2265,7 @@ class NDArray(HasTraits):
             new = self
         else:
             new = self.copy()
+
         if other is None:
             units = None
             if self.units is None:
@@ -2272,56 +2273,68 @@ class NDArray(HasTraits):
             elif force:
                 new._units = None
                 return new
+
         elif isinstance(other, str):
             units = ur.Unit(other)
+
         elif hasattr(other, 'units'):
             units = other.units
+
         else:
             units = ur.Unit(other)
+
         if self.has_units:
+
+            oldunits = self._units
+
+            def _transform(new):
+                # not a linear transform
+                udata = (new.data * new.units).to(units)
+                new._data = udata.m
+                new._units = udata.units
+
+                if new._roi is not None:
+                    roi = (np.array(new._roi) * self.units).to(units)
+                    new._roi = list(udata.m)
+                if self.linear:
+                    # try to make it linear as well
+                    new._linearize()
+                return new
+
             try:
-                if new.meta.larmor:  # _origin in ['topspin', 'nmr']:
-                    # its nmr data
+                if new.meta.larmor:  # _origin in ['topspin', 'nmr']
                     set_nmr_context(new.meta.larmor)
                     with ur.context('nmr'):
-                        q = Quantity(1., self._units).to(units)
-                else:
-                    q = Quantity(1., self._units).to(units)
-
-                scale = q.magnitude
-                new._units = q.units
+                        new = _transform(new)
 
                 # particular case of dimensionless units: absorbance and transmittance
-
-                if self.units in [ur.transmittance, ur.absolute_transmittance]:
+                if oldunits in [ur.transmittance, ur.absolute_transmittance]:
                     if units == ur.absorbance:
-                        new._data = -np.log10(new._data * scale)
-                        scale = 1.
-                        if new.title == 'Transmittance':
-                            new._title = 'Absorbance'
+                        udata = (new.data * new.units).to(units)
+                        new._data = -np.log10(udata.m)
+                        new._units = units
+                        if new.title.lower() == 'transmittance':
+                            new._title = 'absorbance'
 
-                elif self.units == ur.absorbance:
+                elif oldunits == ur.absorbance:
                     if units in [ur.transmittance, ur.absolute_transmittance]:
-                        new._data = 10. ** (-new._data)
-                        if new.title == 'Absorbance':
-                            new._title = 'Transmittance'
-
-                # Now we need to transform the data accordingly
-                # In the case of LinearCoordinate, it would be simple if the new units led to linear
-                # scale, but sometimes it is not the case
-                # for instance when we transform from wavenumber to wavelength.
-                # So we work on the data whatever the case (linear or non linera and we transform later to
-                # linear case if needed.
-
-                linear = self.linear
-
-                if not linear:
-                    new._data = new._data * scale  # new * scale #
+                        scale = Quantity(1., self._units).to(units).magnitude
+                        new._data = 10. ** -new.data * scale
+                        new._units = units
+                        if new.title.lower() == 'absorbance':
+                            new._title = 'transmittance'
                 else:
-                    new._increment = new._increment * scale
-                new._offset = new._offset * scale
-                if new._roi is not None:
-                    new._roi = list(np.array(new._roi) * scale)
+                    # change the title for spectrocopic units change
+                    new = _transform(new)
+                    if (oldunits.dimensionality in ['1/[length]', '[length] ** 2 * [mass] / [time] ** 2'] and
+                        new._units.dimensionality == '1/[time]'):
+                        new._title = 'frequency'
+                    elif (oldunits.dimensionality in ['1/[time]', '[length] ** 2 * [mass] / [time] ** 2'] and
+                          new._units.dimensionality == '1/[length]'):
+                        new._title = 'wavenumbers'
+                    elif (oldunits.dimensionality in ['1/[time]', '1/[length]'] and
+                          new._units.dimensionality == '[length] ** 2 * [mass] / [time] ** 2'):
+                        new._title = 'wavelength'
 
             except DimensionalityError as exc:
                 if force: