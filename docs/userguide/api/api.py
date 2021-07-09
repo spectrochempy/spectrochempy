f6b07fb43952c1f1d0b783189282f8bf65b9ac58
atravert <arnaud.travert@unicaen.fr>
2021-07-09 21:39:58 +0200

    DOC cont'd

--- docs/userguide/api/api.py
+++ docs/userguide/api/api.py
@@ -64,7 +64,7 @@ print(f' description : {scp.description}')
 # | --- | --- |
 # | `DEBUG` / `10` | help diagnose problems on the running process or help developers |
 # | `INFO`  / `20` | general information on the running process |
-# | `WARNING` / `30` | a condition might cause a problem with respect to the expected behaviour |
+# | `WARNING` / `30` (default) | a condition might cause a problem with respect to the expected behaviour |
 # | `ERROR`   / `40` | wrong argument/commands are used or bug |
 # | `CRITICAL` / `50` | the process could lead to a system crash |
 #
@@ -80,20 +80,36 @@ print(f' description : {scp.description}')
 # ```
 # The current loglevel can be obtained with the `scp.get_loglevel()` function.
 #
-# For instance the following code shows how to print and change the current loglevel
+# The following instruction prints the current loglevel
 # %%
 print(f'Default: {scp.get_loglevel()}')       # print the current loglevel
+
+# %% [markdown]
+# As expected, it yields `30` the numerical value corresponding to the `WARNING` level. Now, the next instruction
+# lowers the loglevel to `ÌNFO`:
+# %%
 scp.set_loglevel(scp.INFO)                    # set loglevel to 'INFO'
+
+
+# %% [markdown]
+# We see that the API then delivers the INFO message: `changed default log_level to INFO`.
+#
+# The next instruction prints the current level. As expected, it yields `20` the numerical value corresponding to
+# the `INFO` level:.
+# %%
 print(f'New loglevel: {scp.get_loglevel()}')
+
+# %% [markdown]
+# And  finally, the next instructions reset the loglevel to `WARNING`, and print it.  and return to the default level.
+# As seen below, no message `changed default log_level to ...` is delivered
+
+# %%
 scp.set_loglevel('WARNING')                   #reset to default
 print(f'New loglevel: {scp.get_loglevel()}')
 
 # %% [markdown]
-# As seen above, the INFO message: 'changed default log_level to INFO' has been displayed when the `ÌNFO` level
-# has been set while no such a message is displayed when the `WARNING`level is set.
-#
-# It is also possible to define these messages in scripts:
-# In the cell below, we set the loglevel to `INFO` and try to print two types of messages:
+# It is also possible to issue such messages in scripts. In the cell below, we set the loglevel to `INFO` and try to
+# print two types of messages:
 
 # %%
 scp.set_loglevel('INFO')
@@ -101,10 +117,9 @@ scp.info_('this is an info message!')
 scp.debug_('this is a debug message!')
 
 # %% [markdown]
-# A expected, only the info message was displayed, as expected.
+# A expected, only the info message was displayed.
 #
-#
-# If we change the loglevel to ``DEBUG``, we should get the two messages:
+# If we change the loglevel to ``DEBUG``, then the two messages will be printed:
 
 # %%
 scp.set_loglevel(scp.DEBUG)
@@ -112,7 +127,8 @@ scp.info_('this is an info message!')
 scp.debug_('this is a debug message!')
 
 # %% [markdown]
-# Let's now come back to a standard level of message for the rest of the Tutorial.
+# Let's now come back to the standard level of message for the rest of the Tutorial -- in yhis case neither `DEBUG`
+# nor `INFO` messages will be printed.
 
 # %%
 scp.set_loglevel(scp.WARNING)
@@ -120,19 +136,12 @@ scp.set_loglevel(scp.WARNING)
 scp.info_('this is an info message!')
 scp.debug_('this is a debug message!')
 
-
-# %% [markdown]
-# Other configuration items will be further described when necessary in the other chapters.
-
 # %% [markdown]
 #
 # ### Error handling
-#
-#
-#
 # If something goes wrong with during a cell execution,  a ``traceback`` is displayed.
 #
-# For instance, the object or method ``toto`` does not exist in the API, so an error (**ImportError**) is generated
+# For instance, the object or method ``toto`` does not exist in the API, so an error (`ImportError`) is generated
 # when trying to import this from the API.
 #
 # Here we catch the error with a conventional `try-except` structure
@@ -146,12 +155,10 @@ except ImportError as e:
 # %% [markdown]
 # The error will stop the execution if not catched.
 #
-# This is a basic behavior of python : one way to avoid stoppping the execution without displaying a message is :
+# This is a basic behavior of python : one way to avoid stopping the execution without displaying a message is :
 
 # %%
 try:
     from spectrochempy import toto  # noqa: F811, F401
 except Exception:
     pass
-
-
