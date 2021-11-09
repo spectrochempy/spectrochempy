6fb7162a7be68fa77a041d32b47e5d289266dd6d
LCS112N\Arnaud <arnaud.travert@ensicaen.fr>
2021-11-09 14:08:43 +0100

    DEV IRIS, cont'
    + FIX ndio error

--- tests/test_analysis/test_iris.py
+++ tests/test_analysis/test_iris.py
@@ -2,12 +2,14 @@
 # flake8: noqa
 
 import os
+import pytest
 
 from spectrochempy.analysis.iris import IRIS
 from spectrochempy.core.dataset.coord import Coord
 from spectrochempy.core.dataset.nddataset import NDDataset
 from spectrochempy.utils import show
 
+ #pytestmark = pytest.mark.skip("WIP: iris dev on going")
 
 # import pytest
 # @pytest.mark.skip('do not work with workflow - need to solve this!')
