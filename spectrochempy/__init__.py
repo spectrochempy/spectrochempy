8379a939528dc391982a0d3eb67da9b42e083961
Christian Fernandez <christian.fernandez@ensicaen.fr>
2021-11-27 19:03:50 +0100

    MAINT: Updated Copyright string

--- spectrochempy/__init__.py
+++ spectrochempy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # ======================================================================================================================
-# Copyright (©) 2015-2021 LCS
+# Copyright (©) 2015-2022 LCS
 # Laboratoire Catalyse et Spectrochimie, Caen, France.
 #
 # Authors:
