33cb2164077b1ab59c08a9f06e091ae4a10894e4
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-01-05 14:50:28 +0100

    Fix a typo in make.py  (#339)
    Fix a typo in make.py which prevents display of html/latest pages for release.

--- docs/userguide/processing/alignment.py
+++ docs/userguide/processing/alignment.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.0
+#       jupytext_version: 1.13.5
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
