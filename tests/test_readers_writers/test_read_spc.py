fd4a5e6434e8e6a9d620d012739dc13739f14eac
atravert <arnaud.travert@unicaen.fr>
2022-01-14 12:45:35 +0100

    Cont'

--- tests/test_readers_writers/test_read_spc.py
+++ tests/test_readers_writers/test_read_spc.py
@@ -5,11 +5,8 @@
 from pathlib import Path
 
 import spectrochempy as spc
-from spectrochempy.core import preferences as prefs
-from spectrochempy.core.dataset.nddataset import NDDataset
 
 
 def test_read_spc():
-
     A = spc.read_spc("galacticdata/000001_Spectrum.spc")
-    assert A.description == "Dataset from spc file."
+    assert "Dataset from spc file." in A.description
