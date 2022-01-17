48792dfff85b8cb8906b4d70f94d4ea699d6fbce
atravert <arnaud.travert@unicaen.fr>
2022-01-17 09:12:50 +0100

    cont

--- tests/test_core/test_readers/test_read_spc.py
+++ tests/test_core/test_readers/test_read_spc.py
@@ -4,9 +4,18 @@
 
 from pathlib import Path
 
-import spectrochempy as spc
+import spectrochempy as scp
 
 
 def test_read_spc():
-    A = spc.read_spc("galacticdata/000001_Spectrum.spc")
+
+    A = scp.read_spc("galacticdata/BARBITUATES.spc")
+    # multi file, can't be read yet
+    assert A == []
+
+    B = scp.read_spc("galacticdata/barbsvd.spc")
+    # multi file, can't be read yet
+    assert B == []
+
+    C = scp.read_spc("galacticdata/BARBITUATES.spc")
     assert "Dataset from spc file." in A.description
