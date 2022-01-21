bdaeee43a42b51010341d8a5a4551f6d0d3ff57d
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-01-21 19:03:04 +0100

    DEV: added a show_version method (useful for bug reports)

--- spectrochempy/utils/print_versions.py
+++ spectrochempy/utils/print_versions.py
@@ -1,11 +1,12 @@
 """Utility functions for printing version information."""
-import importlib
+
 import locale
 import os
 import platform
 import struct
 import subprocess
 import sys
+from spectrochempy import optional
 
 
 def get_sys_info():
@@ -59,24 +60,6 @@ def get_sys_info():
     return blob
 
 
-def netcdf_and_hdf5_versions():
-    libhdf5_version = None
-    libnetcdf_version = None
-    try:
-        import netCDF4
-
-        libhdf5_version = netCDF4.__hdf5libversion__
-        libnetcdf_version = netCDF4.__netcdf4libversion__
-    except ImportError:
-        try:
-            import h5py
-
-            libhdf5_version = h5py.version.hdf5_version
-        except ImportError:
-            pass
-    return [("libhdf5", libhdf5_version), ("libnetcdf", libnetcdf_version)]
-
-
 def show_versions(file=sys.stdout):
     """print the versions of xarray and its dependencies
 
@@ -85,78 +68,34 @@ def show_versions(file=sys.stdout):
     file : file-like, optional
         print to the given file-like object. Defaults to sys.stdout.
     """
-    sys_info = get_sys_info()
 
-    try:
-        sys_info.extend(netcdf_and_hdf5_versions())
-    except Exception as e:
-        print(f"Error collecting netcdf / hdf5 version: {e}")
+    print("\nINSTALLED VERSIONS")
+    print("------------------")
 
-    deps = [
-        # (MODULE_NAME, f(mod) -> mod version)
-        ("xarray", lambda mod: mod.__version__),
-        ("pandas", lambda mod: mod.__version__),
-        ("numpy", lambda mod: mod.__version__),
-        ("scipy", lambda mod: mod.__version__),
-        # xarray optionals
-        ("netCDF4", lambda mod: mod.__version__),
-        ("pydap", lambda mod: mod.__version__),
-        ("h5netcdf", lambda mod: mod.__version__),
-        ("h5py", lambda mod: mod.__version__),
-        ("Nio", lambda mod: mod.__version__),
-        ("zarr", lambda mod: mod.__version__),
-        ("cftime", lambda mod: mod.__version__),
-        ("nc_time_axis", lambda mod: mod.__version__),
-        ("PseudoNetCDF", lambda mod: mod.__version__),
-        ("rasterio", lambda mod: mod.__version__),
-        ("cfgrib", lambda mod: mod.__version__),
-        ("iris", lambda mod: mod.__version__),
-        ("bottleneck", lambda mod: mod.__version__),
-        ("dask", lambda mod: mod.__version__),
-        ("distributed", lambda mod: mod.__version__),
-        ("matplotlib", lambda mod: mod.__version__),
-        ("cartopy", lambda mod: mod.__version__),
-        ("seaborn", lambda mod: mod.__version__),
-        ("numbagg", lambda mod: mod.__version__),
-        ("fsspec", lambda mod: mod.__version__),
-        ("cupy", lambda mod: mod.__version__),
-        ("pint", lambda mod: mod.__version__),
-        ("sparse", lambda mod: mod.__version__),
-        # xarray setup/test
-        ("setuptools", lambda mod: mod.__version__),
-        ("pip", lambda mod: mod.__version__),
-        ("conda", lambda mod: mod.__version__),
-        ("pytest", lambda mod: mod.__version__),
-        # Misc.
-        ("IPython", lambda mod: mod.__version__),
-        ("sphinx", lambda mod: mod.__version__),
-    ]
-
-    deps_blob = []
-    for (modname, ver_f) in deps:
+    for key, val in get_sys_info():
+        print(f"{key}: {val}")
+    print()
+    deps = []
+    with open("../../environment.yml", "r") as f:
+        start = False
+        for dep in f.readlines():
+            if "dependencies" in dep:
+                start = True
+            if not start:
+                continue
+            if dep.strip().startswith("-"):
+                dep = dep.strip()[2:].split("<")[0].split(">")[0].split("=")[0]
+                if dep != "python":
+                    deps.append(dep)
+    deps.append("spectrochempy")
+    for dep in deps:
+        mod = optional.import_optional_dependency(dep, errors="ignore")
         try:
-            if modname in sys.modules:
-                mod = sys.modules[modname]
-            else:
-                mod = importlib.import_module(modname)
-        except Exception:
-            deps_blob.append((modname, None))
-        else:
-            try:
-                ver = ver_f(mod)
-                deps_blob.append((modname, ver))
-            except Exception:
-                deps_blob.append((modname, "installed"))
-
-    print("\nINSTALLED VERSIONS", file=file)
-    print("------------------", file=file)
-
-    for k, stat in sys_info:
-        print(f"{k}: {stat}", file=file)
-
-    print("", file=file)
-    for k, stat in deps_blob:
-        print(f"{k}: {stat}", file=file)
+            print(
+                f"{dep}: {optional.get_module_version(mod) if mod is not None else None}"
+            )
+        except ImportError:
+            print(f"{dep}: (Can't determine version string)")
 
 
 if __name__ == "__main__":
