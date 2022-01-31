e26e3b305a6eee897c30ed2cbede17091443d559
atravert <arnaud.travert@unicaen.fr>
2022-01-31 10:25:23 +0100

    add few explanations on PCA iris example

--- docs/gettingstarted/examples/analysis/plot_pca_iris.py
+++ docs/gettingstarted/examples/analysis/plot_pca_iris.py
@@ -7,8 +7,8 @@
 """
 PCA analysis example
 ---------------------
-In this example, we perform the PCA dimensionality reduction of the classical
-``iris`` dataset.
+In this example, we perform the PCA dimensionality reduction of the classical ``iris`` dataset (Ronald A. Fisher.
+"The Use of Multiple Measurements in Taxonomic Problems. Annals of Eugenics, 7, pp.179-188, 1936).
 
 """
 
@@ -29,31 +29,36 @@ except (IOError, OSError):
 pca = scp.PCA(dataset, centered=True)
 
 ##############################################################
-# Reduce the data to a lower dimensionality (the number of
-# components is automatically determined)
+# Reduce the data to a lower dimensionality. Here, the number of
+# components is automatically determined using `n_pc="auto"`. As
+# indicated by the dimension of LT, 4 PC are found.
 
 S, LT = pca.reduce(n_pc="auto")
 
 print(LT)
 
 ###############################################################
-# Print explained and cumulative variance:
+# The figures of merit (explained and cumulative variance) confirm that
+# these 4 PC's explain 100% of the variance:
 #
 pca.printev()
 
 ###############################################################
-# And display the results graphically
+# These figures od merit can also be displayed graphically
 #
 # The ScreePlot
 _ = pca.screeplot()
 
 ########################################################################################################################
-# ScorePlot of 2 PC's
+# The score plots can be used for classification purposes. The first one - in 2D for the 2 first PC's - shows that the first PC
+# allows distinguishing Iris-setosa (PC#1 < -1) from other species (PC#1 > -1), while more PC's are required
+# to distinguish versicolor from viginica.
 _ = pca.scoreplot(1, 2, color_mapping="labels")
 
 ########################################################################################################################
-# or in 3D for 3 PC's
-_ = pca.scoreplot(1, 2, 3, color_mapping="labels")
-
+# The second one - in 3D for the 3 first PC's - indicates that a thiord PC won't allow better dsitinguishing versicolor
+# from viginica. .
+ax = pca.scoreplot(1, 2, 3, color_mapping="labels")
+ax.view_init(10, 75)
 
 # scp.show()  # uncomment to show plot if needed (not necessary in jupyter notebook)
