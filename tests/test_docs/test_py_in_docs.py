e95ce4b6ccb00c554ccf482a20d4e1d17c9c0230
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-02-03 22:35:00 +0100

    FIX: stupid typos

--- tests/test_docs/test_py_in_docs.py
+++ tests/test_docs/test_py_in_docs.py
@@ -19,7 +19,7 @@ import pytest
 # Uncomment to avoid these long tests which are also done in docs
 # pytestmark = pytest.mark.skip(reason="check when building docs in CI")
 pytestmark = pytest.mark.skipif(
-    "sys.platform" == "win32" or sys.version_info < (3, 9),
+    sys.platform == "win32" or sys.version_info < (3, 9),
     reason="1) Does not work on windows, 2) Execute this long run only one time on github workflow",
 )
 
