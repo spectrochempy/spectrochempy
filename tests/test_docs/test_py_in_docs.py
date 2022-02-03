ae2cd49b3f15723e520130c0b03df8c63a3a6aa8
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-02-03 21:58:31 +0100

    FIX: Only one pytestmark in the module!

--- tests/test_docs/test_py_in_docs.py
+++ tests/test_docs/test_py_in_docs.py
@@ -19,11 +19,8 @@ import pytest
 # Uncomment to avoid these long tests which are also done in docs
 # pytestmark = pytest.mark.skip(reason="check when building docs in CI")
 pytestmark = pytest.mark.skipif(
-    "sys.platform" == "win32", reason="seems that it does not work on windows"
-)
-pytestmark = pytest.mark.skipif(
-    sys.version_info < (3, 9),
-    reason="want to execute this long run only one tume on github workflow",
+    "sys.platform" == "win32" or sys.version_info < (3, 9), 
+    reason="1) Does not work on windows, 2) Execute this long run only one time on github workflow"
 )
 
 from pathlib import Path
