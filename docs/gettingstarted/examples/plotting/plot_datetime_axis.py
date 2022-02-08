141807ce22403c236ac02fb7deca8e459d8232b6
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-02-08 19:10:46 +0100

    DOCS: Cleaning example plot_datetime_axis

--- docs/gettingstarted/examples/plotting/plot_datetime_axis.py
+++ docs/gettingstarted/examples/plotting/plot_datetime_axis.py
@@ -13,101 +13,97 @@ Plotting with datetime axis
 # %%
 import spectrochempy as scp
 
-# %% [markdown]
+# %%
 # The location of the spectrochempy_data can be found in preferences
 
-# %%
 datadir = scp.preferences.datadir
 
-# %% [markdown]
-# Let's read on of the dataset (in `spg` Omnnic format)
-
 # %%
+# Let's read on of the dataset (in `spg` Omnic format)
+
 dataset = scp.NDDataset.read_omnic(datadir / "irdata" / "nh4y-activation.spg")
-
-# %% [markdown]
-# We can have a pretty print of the dataset using the `print_`method. Note that in a
-# jupyter notebook you just need to write
-#  ```dataset```
-# and an output in a html format will be issued.
+dataset
 
 # %%
-scp.print_(dataset)
-
-# %% [markdown]
-# This dataset is actually to dimensional: one dimension (x) is the wavenumber axis (in $cm^1$ units),
+# This dataset is actually to dimensional: one dimension (x) is the wavenumber axis
+# (in $cm^1$ units),
 # the second one is the acquisition datetime axis. The intensity is in a dimensionless
 # units: absorbance.
 
-# %% [markdown]
+# %%
 # By default it is plotted with the stack method
 
-# %%
 _ = dataset.plot(figsize=(6, 4))
 
-# %% [markdown]
+# %%
 # Previously plotted as a stack plot, this dataset can also be plotted
 # using a contour plot (method: map)
 
-# %%
 _ = dataset.plot_map(figsize=(6, 4))
 
-# %% [markdown]
+# %%
 # As you see, spectrochempy does not display the datetimes on the vertical axis,
-# because in this case the interval betwwen the date is relatively small. So it s displayed in seconds, with the starting reference noted on the aixs label.
+# because in this case the interval betwwen the date is relatively small.
+# So it s displayed in seconds, with the starting reference noted on the aixs label.
 
-# %% [markdown]
+# %%
 # Let's see another display of the datetime, that is in colorbar:
 
 # %%
 _ = dataset.plot(figsize=(6, 4), colorbar=True, colormap="magma")
 
-# %% [markdown]
+# %%
 # It is also displayed on 1D sections:
 
 # %%
 section = dataset[:, 3200.0]
 _ = section.plot(figsize=(6, 3), c="red", lw=3)
 
-# %% [markdown]
-# ## How to change the time units?
+# %%
+# How to change the time units?
+# -----------------------------
 
-# %% [markdown]
-# It may be desirable to change the default units on the datetime axis, *e.g.,* from seconds to hours in the above example.
+# %%
+# It may be desirable to change the default units on the datetime axis,
+# *e.g.,* from seconds to hours in the above example.
 
-# %% [markdown]
-# ### Solution 1.
-# The first solution, if we don't need to keep reference to the reference date, it to transform them in timedelta values:
+# %%
+# Solution 1
+# ++++++++++
+#
+# The first solution, if we don't need to keep reference to the reference date,
+# it to transform them in timedelta values:
 
 # %%
 newdataset = (
     dataset.copy()
 )  # we copy it as we will need the original dataset for solution 2
+
 newdataset.y -= newdataset.y[0]
 scp.print_(newdataset.y)
 
-# %% [markdown]
-# This way we have subtracted the first value of the y axis array to all other values. The values are now in units of seconds. To change this in hours for instance, just do this:
-
 # %%
+# This way we have subtracted the first value of the y axis array to all other values.
+# The values are now in units of seconds. To change this in hours for instance,
+# just do this:
+
 newdataset.y.ito("hours")
 scp.print_(newdataset.y)
 
-# %% [markdown]
+# %%
 # And now the result is:
 
-# %%
 _ = newdataset.plot_map(figsize=(6, 4))
 
-# %% [markdown]
-# ### Solution 2.
-#
-# Another solution is to pass the keword parameters `time_units` in the plot command. This time_units must be a string among : ["days", "hours", "minute", "second", "millisecond", "microsecond", "nanosecond"]
-
 # %%
+# Solution 2
+# ++++++++++
+#
+# Another solution is to pass the keword parameters `time_units` in the plot command.
+# This time_units must be a string among : ["days", "hours", "minute", "second", "millisecond", "microsecond", "nanosecond"]
+
 _ = dataset.plot_map(figsize=(6, 4), time_units="minutes")
 
 # %%
-_ = section.plot(figsize=(6, 3), time_units="hours", c="red", lw=3)
 
-# %%
+_ = section.plot(figsize=(6, 3), time_units="hours", c="red", lw=3)
