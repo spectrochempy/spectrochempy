a7e7e0b0353b5b539eb6896632e4034c6909eb6b
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-02-09 09:38:14 +0100

    MAINT: modified workflow to import xarray for tests and docs building.
    This is an optional library for now, but is needed here for full testing.

---
+++ docs/gettingstarted/examples/read/plot_readwrite.py
@@ -0,0 +1,28 @@
+#  =====================================================================================
+#  Copyright (©) 2015-2022 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.
+#  CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
+#  See full LICENSE agreement in the root directory.
+#  =====================================================================================
+
+"""
+Loading Bruker OPUS files
+============================================
+
+Here we load an experimental Bruker OPUS files and plot it.
+
+"""
+
+import spectrochempy as scp
+
+Z = scp.read_opus(
+    ["test.0000", "test.0001", "test.0002", "test.0003"], directory="irdata/OPUS"
+)
+print(Z)
+
+# %%
+# plot it
+
+Z.plot()
+
+
+scp.show()  # uncomment to show plot if needed (not necessary in jupyter notebook)
