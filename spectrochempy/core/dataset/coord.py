fff0d81a7ce60ef6c1ca2c206907e5d3800df359
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-02-26 16:05:41 +0100

    FIX: complete last commmit

--- spectrochempy/core/dataset/coord.py
+++ spectrochempy/core/dataset/coord.py
@@ -1079,7 +1079,7 @@ class LinearCoord(Coord):
 
         elif not self.linear:
             # in case it was not already a linear array
-            self.offset = offset
+            self.offset = np.round(offset, self._decimals)
             self.increment = np.round(increment, self._decimals)
             self._linear = True
 
