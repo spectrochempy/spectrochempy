4654195c4952927a2ec7770e992f8adcd42f3f65
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-03-14 09:21:47 +0100

    MAINT: set minimum version of pint to 0.18

--- setup.py
+++ setup.py
@@ -155,7 +155,7 @@ setup_args = dict(
         "numba==0.55.0",
         "numpy==1.21.5",
         "orderedset",
-        "pint",
+        "pint>=0.18",
         "plotly",
         "requests",
         "scipy==1.7.3",
