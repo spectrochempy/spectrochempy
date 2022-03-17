8a35bc6444a467c847584eeb3cb679d596f4b568
Christian Fernandez <christian.fernandez@ensicaen.fr>
2022-03-17 08:44:02 +0100

    Automatically update reference data in citing.rst (#427)
    * FIX: Cleaning reference/api.rst
    
    * DOCS: automatically update references data in citing.rst
    
    * MAINT: unpin sphinx

--- docs/userguide/units/units.py
+++ docs/userguide/units/units.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.5
+#       jupytext_version: 1.13.7
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
