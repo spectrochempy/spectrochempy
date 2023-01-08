2aa5a6e6ad1a05ad53e856ab6245cf212de73eff
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-01-08 14:50:30 +0100

    cont.

--- docs/make.py
+++ docs/make.py
@@ -66,11 +66,12 @@ __all__ = []
 
 # create the testdata directory
 import spectrochempy as scp
-from spectrochempy.utils.file import download_testdata
+
+# from spectrochempy.utils.file import download_testdata
 
 datadir = scp.preferences.datadir
 # this process is relatively long, so we do not want to do it several time:
-download_testdata()
+# download_testdata()   (download done using mamba install spectrochempy_data)
 
 
 class BuildDocumentation(object):
