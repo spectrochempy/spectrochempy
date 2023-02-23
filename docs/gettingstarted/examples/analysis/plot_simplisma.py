b0d9e8bfc89323798ac2cfb0ae19ac57569eda9a
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-02-23 12:22:22 +0100

    DOC: Add a SIMPLISMA example for docs

---
+++ docs/gettingstarted/examples/analysis/plot_simplisma.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# ======================================================================================
+# Copyright (©) 2015-2023 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.
+# CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
+# See full LICENSE agreement in the root directory.
+# ======================================================================================
+# flake8: noqa
+"""
+NDDataset SIMPLISMA example
+-------------------------------
+In this example, we perform the PCA dimensionality reduction of a spectra
+dataset
+
+"""
+# %%
+# Import the spectrochempy API package (and the SIMPLISMA model independently)
+import spectrochempy as scp
+
+# %%
+# Load a matlab datasets
+print("Dataset (Jaumot et al., Chemometr. Intell. Lab. 76 (2005) 101-110)):")
+lnd = scp.read_matlab("matlabdata/als2004dataset.MAT", merge=False)
+for mat in lnd:
+    print("    " + mat.name, str(mat.shape))
+
+ds = lnd[-1]
+_ = ds.plot()
+
+# %%
+# Add some metadata for a nicer display
+ds.title = "absorbance"
+ds.units = "absorbance"
+ds.set_coordset(None, None)
+ds.y.title = "elution time"
+ds.x.title = "wavelength"
+ds.y.units = "hours"
+ds.x.units = "nm"
+
+# %%
+# Fit the SIMPLISMA model
+print("Fit SIMPLISMA on {}\n".format(ds.name))
+simpl = scp.SIMPLISMA(max_components=20, tol=0.2, noise=3, log_level="INFO")
+simpl.fit(ds)
+
+# %%
+# Plot concentration
+_ = simpl.C.T.plot(title="Concentration")
+
+# %%
+# Plot components (St)
+_ = simpl.components.plot(title="Components")
+
+# %%
+# Show the plot of merit
+# after reconstruction oto the original data space
+X_hat = simpl.inverse_transform()
+simpl.plotmerit(ds, X_hat)
+
+# %%
+# uncomment the line below to see plot if needed (not necessary in jupyter notebook)
+# scp.show()
