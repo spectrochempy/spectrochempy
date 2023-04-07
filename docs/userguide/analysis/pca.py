11deec57246924291c896f0272123f7a63f7f3d9
atravert <arnaud.travert@ensicaen.fr>
2023-04-07 17:59:21 +0200

    fix tutorial pca

--- docs/userguide/analysis/pca.py
+++ docs/userguide/analysis/pca.py
@@ -24,7 +24,7 @@
 #     version: 3.10.8
 # ---
 # %% [markdown]
-# # PCA and SVD
+# # Principal Component Analysis
 
 # %%
 import spectrochempy as scp
@@ -35,9 +35,7 @@ import spectrochempy as scp
 # PCA (standing for Principal Component Analysis) is one of the most popular
 # method for dimensionality reduction by factorising a dataset $X$ into score ($S$) and
 # loading ($L^T$) matrices of a limited number of components and minimize the error $E$:
-#
-# $$ X = S L^T + E $$*
-#
+# $$ X = S L^T + E $$
 # These matrices are such that the product of the first column of $S$ - the score vector $s_1$ -
 # by the first line of $L^T$ - the loading vector $l_1$ - are those that best explain the variance
 # of the dataset. These score and loading vectors are together called the ‘first component’. The
@@ -48,20 +46,18 @@ import spectrochempy as scp
 # with similar methods and attributes on the one hands, and some that are specific to spectrochempy.
 #
 # ## Loading of the dataset
-# Here we show how PCA is implemented in Scpy on a dataset corresponding to # a HPLC-DAD run,
-# from Jaumot et al. Chemolab, 76 (2005), # pp. 101-110 and Jaumot et al. Chemolab, 140 (2015)
-# pp. 1-12. This dataset (and others) # can be loaded from the # "Multivariate Curve Resolution
-# Homepage" at https://mcrals.wordpress.com/download/example-data-sets. For the user convenience,
+# Here we show how PCA is implemented in Scpy on a dataset corresponding to a HPLC-DAD run,
+# from Jaumot et al. Chemolab, 76 (2005), pp. 101-110 and Jaumot et al. Chemolab, 140 (2015)
+# pp. 1-12. This dataset (and others) can be loaded from the [Multivariate Curve Resolution
+# Homepage](https://mcrals.wordpress.com/download/example-data-sets). For the user convenience,
 # this dataset is present in the 'datadir' of spectrochempy in 'als2004dataset.MAT' and can be
 # read as follows in Scpy:
-
 # %%
 A = scp.read_matlab("matlabdata/als2004dataset.MAT")
 
 # %% [markdown]
 # The .mat file contains 6 matrices which are thus returned in A as a list of 6
 # NDDatasets. We print the names and dimensions of these datasets:
-
 # %%
 for a in A:
     print(f"{a.name} : {a.shape}")
@@ -72,7 +68,7 @@ for a in A:
 # As usual, the rows correspond to the 'time axis' of the HPLC run(s), and the columns
 # to the 'wavelength' axis of the UV spectra.
 #
-# Let's name it 'X' (as in the matrix equation above), display its content and plot it:
+# Let's name it `X` (as in the matrix equation above), display its content and plot it:
 
 # %%
 X = A[-1]
@@ -88,7 +84,6 @@ _ = X.plot()
 # For the clarity of the tutorial, we add: (i) a proper title to the data, (ii)
 # the default coordinates (index) do the NDDataset and (iii) a proper name for these
 # coordinates:
-
 # %%
 X.title = "absorbance"
 X.y = scp.Coord.arange(51, title="elution time")
@@ -97,14 +92,14 @@ X.x = scp.Coord.arange(96, title="wavelength")
 # %% [markdown]
 # From now on, these names will be taken into account by Scpy in the plots as well as
 # in the analysis treatments (PCA, EFA, MCR-ALS ...). For instance to plot X as a surface:
-
 # %%
 surf = X.plot_surface(colorbar=True, linewidth=0.2, ccount=100, figsize=(10, 5))
 
 # %% [markdown]
 # ## PCA
 #
-# # First, we create a PCA object with default parameters and we compute the components with the fit() method:
+# First, we create a PCA object with default parameters and we compute the components with the fit() method:
+# %%
 pca = scp.PCA()
 pca.fit(X)
 
@@ -112,22 +107,23 @@ pca.fit(X)
 # The default number of components is given by min(X.shape). As often in spectroscopy
 # the number of observations/spectra is lower that the number of wavelength/features, the number of components
 # often equals the number of spectra.
-
+# %%
 pca.n_components
 
 # %% [markdown]
 # As the main purpose of PCA is dimensionality reduction, we generally limit the PCA to a limited number of components.
 # This can be done by either reseting the number of components of an existing object:
-
+# %%
 pca.used_components = 8
 pca.fit(X)
 
 # %% [markdown]
 # Or directly by creating a PCA instance with the desired number of components:
+# %%
 pca = scp.PCA(used_components=8)
 pca.fit(X)
 
-# %%
+# %% [markdown]
 # The choice of the optimum number of components to describe a dataset is always a delicate matter. It can be based on:
 # - examination of the explained variance
 # - examination of the scores and loadings
@@ -139,7 +135,7 @@ pca.printev()
 # %% [markdown]
 # The data of the two last columns are stored in the `PCA.explained_variance_ratio' and
 # `PCA.cumulative_explained_variance' attributes. They can be plotted directly as a scree plot:
-
+# %%
 _ = pca.screeplot()
 
 # %% [markdown]
@@ -153,6 +149,7 @@ _ = pca.screeplot()
 #
 # Scores and Loadings can be plotted using the usual plot() method, with prior transpositon
 # for the scores:
+# %%
 pca.scores.T.plot()
 pca.loadings.plot()
 
@@ -164,18 +161,17 @@ pca.loadings.plot()
 # Another possibility can be a visual comparison of the modeled dataset $\hat{X} = S L^T $,
 # the original dataset $X$ and the resitua,s $E = X - \hat{X}$. This can be done using
 # the `plotmerit()` method which plots both $X$, $\hat{X}$ (in dotted lines) and the residuals (in red):
-#
+# %%
 pca = scp.PCA(used_components=4)
 pca.fit(X)
 pca.plotmerit()
 
 # %% [markdown]
 # The number of spectra can be limited by the `nb_traces` attributes:
-#
+# %%
 pca.plotmerit(nb_traces=5)
-
 
 # %% [markdown]
 # and if needed both datasets can be shifted using the `offset` attribute (in percet of the fullscale):
-#
+# %%
 pca.plotmerit(nb_traces=5, offset=100.0)
