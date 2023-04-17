78ce2f39384979a50d3db613422745a058d42fd6
atravert <arnaud.travert@ensicaen.fr>
2023-04-17 23:27:06 +0200

    cont

--- docs/examples/analysis/plot_pls.py
+++ docs/examples/analysis/plot_pls.py
@@ -8,112 +8,57 @@
 """
 NDDataset PCA analysis example
 -------------------------------
-In this example, we perform a PLS regression of corn spectra to predict
-dataset
-
+In this example, we perform a PLS regression to predict the composition of corn samples
+from their NIR spectra.
 """
+
 # %%
 # Import the spectrochempy API package
 import spectrochempy as scp
+import matplotlib.pyplot as plt
 
 # %%
 # Load a dataset
-dataset = scp.read_remote("http://www.eigenvector.com/data/Corn/corn.mat")
-
+ds_list = scp.download("http://www.eigenvector.com/data/Corn/corn.mat")
+for ds in ds_list:
+    print(ds.name)
 # %%
-# The dataset named `'m5spec'`, corresponding to the 80 spectra
-# Let's name assign these specta to `X` , add few informations about the x-scale and plot it:
+# The dataset named `'m5spec'`, contains the NIR spectra of 80 corn samples recorded on the same
+# instrument. Let's assign this NDDataset specta to `X`, add few informations and plot it:
 # %%
-X = A[-3]
-X.title = "absorbance"
+X = ds_list[4]
+X.title = "reflectance"
 X.x.title = "Wavelength"
 X.x.units = "nm"
 X.plot(cmap=None)
 
-#
-# Let's plot the properties of the sample:
-Y = A[3]
+# %%
+# The values of the properties we want to predict are in the `'propval'` dataset:
+Y = ds_list[3]
 _ = Y.T.plot(cmap=None, legend=Y.x.labels)
 
-Y_std = (Y - Y.mean(dim=0)) / Y.std(dim=0)
-_ = Y_std.T.plot(cmap=None, legend=Y.x.labels)
+# We are interested to predict the moisture content:
+y = Y[:, 0]
 
-n_targets = Y.shape[1]
-# %%
-# Create a PCA object and fit the dataset so that the explained variance is greater or
-# equal to 99.9%
-pca = scp.PCA(used_components=0.999)
-pca.fit(dataset)
+# We select arbitrarily the 57 first samples to train the model and the 27 remaining ones
+# to test the model.
+X_train = X[:57]
+X_test = X[57:]
+y_train = y[:57]
+y_test = y[57:]
 
-# %%
-# The number of fitted components is given by the n_components attribute
-# (We obtain 23 components)
-pca.n_components
 
 # %%
-# Transform the dataset to a lower dimensionality using all the fitted components
-scores = pca.transform()
-scores
+# Create a PLS object and fit the train datasets:
+pls = scp.PLS(used_components=5)
+pls.fit(X_train, y_train)
+
+y_train_hat = pls.predict()
+y_test_hat = pls.predict(X_test)
+
+plt.figure()
+plt.plot(y_train.data, y_train_hat.data, "o")
+plt.plot(y_test.data, y_test_hat.data, "x")
+
 
 # %%
-# Finally, display the results graphically
-# ScreePlot
-_ = pca.screeplot()
-
-# %%
-# Score Plot
-_ = pca.scoreplot(scores, 1, 2)
-
-# %%
-# Score Plot for 3 PC's in 3D
-_ = pca.scoreplot(scores, 1, 2, 3)
-
-# %%
-# Displays 4 loadings
-_ = pca.loadings[:4].plot(legend=True)
-
-# %%
-# Here we do a masking of the saturated region between 882 and 1280 cm^-1
-dataset[
-    :, 882.0:1280.0
-] = scp.MASKED  # remember: use float numbers for slicing (not integer)
-_ = dataset.plot()
-
-# %%
-# Apply the PCA model
-pca = scp.PCA(used_components=0.999)
-pca.fit(dataset)
-pca.n_components
-
-# %%
-# As seen above, now only 4 components instead of 23 are necessary to 99.9% of
-# explained variance.
-_ = pca.screeplot()
-
-# %%
-# Displays the loadings
-_ = pca.loadings.plot(legend=True)
-
-# %%
-# Let's plot the scores
-scores = pca.transform()
-_ = pca.scoreplot(scores, 1, 2)
-
-# %% labeling scoreplot with spectra labels
-# Our dataset has already two columns of labels for the spectra but there are little
-# too long for display on plots.
-scores.y.labels
-
-# %%
-# So we define some short labels for each component, and add them as a third column:
-labels = [lab[:6] for lab in dataset.y.labels[:, 1]]
-scores.y.labels = labels  # Note this does not replace previous labels,
-# but adds a column.
-
-# %%
-# now display thse
-_ = pca.scoreplot(scores, 1, 2, show_labels=True, labels_column=2)
-
-# %%
-# uncomment the line below to see plot if needed (not necessary in jupyter notebook)
-scp.show()
