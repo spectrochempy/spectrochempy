ddfe14060853dc1a91ff89cae4949133a883ab59
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-04-18 06:52:23 +0200

    CI: back to ubuntu test only
    Window test need more work regarding logging

--- spectrochempy/application/application.py
+++ spectrochempy/application/application.py
@@ -696,9 +696,7 @@ you are kindly requested to cite it this way: <pre>{cite}</pre></p>.
                 "rotatingfile": {
                     "class": "logging.handlers.RotatingFileHandler",
                     "level": "DEBUG",
-                    "filename": str(
-                        _get_log_dir() / f"spectrochempy_{sys.version[:4]}.log"
-                    ),
+                    "filename": str(_get_log_dir() / "spectrochempy.log"),
                     "maxBytes": 262144,
                     "backupCount": 5,
                 },
