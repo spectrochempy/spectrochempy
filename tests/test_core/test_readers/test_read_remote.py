b0385df33f157b43f9dc75ca4c79f3e6e67a39c2
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-04-19 10:39:55 +0200

    FIX: modify last commit

--- tests/test_core/test_readers/test_read_remote.py
+++ tests/test_core/test_readers/test_read_remote.py
@@ -21,12 +21,12 @@ def test_read_remote():
     filename = IRDATA / "nh4y-activation.spg"
 
     # read normally
-    nd1 = NDDataset.read_omnic(filename)
-    assert str(nd1) == "NDDataset: [float64] a.u. (shape: (y:55, x:5549))"
+    # nd1 = NDDataset.read_omnic(filename)
+    # assert str(nd1) == "NDDataset: [float64] a.u. (shape: (y:55, x:5549))"
 
-    # move the files to simulate their absence:
-    filesaved = filename.with_suffix(".nonexist")
-    filename.rename(filesaved)
+    # delete the file to simulate their absence:
+    filename.unlink()
+
     # now try to read
     nd2 = NDDataset.read_omnic("irdata/nh4y-activation.spg")
     assert str(nd2) == "NDDataset: [float64] a.u. (shape: (y:55, x:5549))"
