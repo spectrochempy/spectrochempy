3d5f8000b0bf03f95bd4b308a0aaee29b18887bd
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-05-20 09:22:41 +0200

    MAINT : some cleaning and reorganisation (application and tests)

--- tests/test_application/test_application.py
+++ tests/test_application/test_application.py
@@ -10,16 +10,6 @@ import logging
 import spectrochempy as scp
 
 
-def test_datadir():
-    # test print a listing of the testdata directory
-    from spectrochempy.application import DataDir
-
-    print(DataDir().listing())
-    # or simply
-    print(DataDir())
-    assert str(DataDir()).startswith("testdata")
-
-
 def test_version():
     # test version
     assert len(scp.version.split(".")) >= 3
@@ -51,33 +41,3 @@ def test_log():
     assert (
         "DEBUG | a second DEBUG info!" not in log_out
     ), " handler[1] is always at level INFO."
-
-
-def test_magic_addscript(ip):
-    if ip is None:
-        scp.warning_("ip is None - pss this test ")
-        return
-
-    from spectrochempy.application import SpectroChemPyMagics
-
-    ip.register_magics(SpectroChemPyMagics)
-
-    ip.run_cell("from spectrochempy import *")
-
-    assert "preferences" in ip.user_ns.keys()
-
-    ip.run_cell("print(preferences.available_styles)", store_history=True)
-    ip.run_cell("project = Project()", store_history=True)
-    x = ip.run_line_magic("addscript", "-p project -o prefs -n preferences 2")
-
-    print("x", x)
-    assert x.strip() == "Script prefs created."
-
-    # with cell contents
-    x = ip.run_cell(
-        "%%addscript -p project -o essai -n preferences\n"
-        "print(preferences.available_styles)"
-    )
-
-    print("result\n", x.result)
-    assert x.result.strip() == "Script essai created."
