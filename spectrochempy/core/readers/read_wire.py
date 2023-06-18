883db56a1a5601a9ca01011f0821a1c76dc1f738
Christian Fernandez <christian.fernandez@ensicaen.fr>
2023-06-18 09:34:28 +0200

    DEV: incorporate code from py_wdf_reader
    This package will become un-maintened soon. But as the licence is MIT, it is compatible with CeCill

--- spectrochempy/core/readers/read_wire.py
+++ spectrochempy/core/readers/read_wire.py
@@ -14,11 +14,11 @@ __dataset_methods__ = __all__
 import datetime
 
 import numpy as np
-from renishawWiRE import WDFReader
 
 from spectrochempy.core.dataset.baseobjects.meta import Meta
 from spectrochempy.core.dataset.coord import Coord
 from spectrochempy.core.readers.importer import Importer, _importer_method, _openfid
+from spectrochempy.extern.py_wdf_reader import WDFReader
 from spectrochempy.utils.docstrings import _docstring
 
 # ======================================================================================
@@ -61,60 +61,6 @@ read_wdf = read_wire
 
 
 # ======================================================================================
-# Private Class
-# ======================================================================================
-class _WDFReader(WDFReader):
-    # WDFReader is a class from the renishawWiRE package (MIT Licence - see
-    # License in the root directory) It requires a filename
-    # to be passed to the constructor. We want to be able to pass a file object.
-    # So, we make a subclass of WDFReader, so we can modify the __init__ method.
-
-    def __init__(self, file_obj):
-        # This code is adapted from the WDFReader class (licence MIT),
-        # except that we use the file_obj directly in the input.
-
-        self.file_obj = file_obj
-
-        # Initialize the properties for the wdfReader class
-        self.title = ""
-        self.username = ""
-        self.measurement_type = None
-        self.scan_type = None
-        self.laser_length = None
-        self.count = None
-        self.spectral_unit = None
-        self.xlist_type = None
-        self.xlist_unit = None
-        self.ylist_type = None
-        self.ylist_unit = None
-        self.point_per_spectrum = None
-        self.data_origin_count = None
-        self.capacity = None
-        self.application_name = ""
-        self.application_version = [None] * 4
-        self.xlist_length = 0
-        self.ylist_length = 0
-        self.accumulation_count = None
-        self.block_info = {}  # each key has value (uid, offset, size)
-        self.is_completed = False
-        self.debug = False
-
-        # Parse the header section in the wdf file
-        self.__locate_all_blocks()
-        # Parse individual blocks
-        self.__treat_block_data("WDF1")
-        self.__treat_block_data("DATA")
-        self.__treat_block_data("XLST")
-        self.__treat_block_data("YLST")
-        self.__treat_block_data("ORGN")
-        self.__treat_block_data("WMAP")
-        self.__treat_block_data("WHTL")
-
-        # Reshape spectra after reading mapping information
-        self.__reshape_spectra()
-
-
-# ======================================================================================
 # Private functions
 # ======================================================================================
 @_importer_method
@@ -125,7 +71,7 @@ def _read_wdf(*args, **kwargs):
 
     fid, kwargs = _openfid(filename, **kwargs)
 
-    reader = _WDFReader(fid)
+    reader = WDFReader(fid)
 
     meta = Meta()
     for item in (
@@ -183,21 +129,21 @@ def _read_wdf(*args, **kwargs):
     elif mtype == 3:
         data = np.array(reader.spectra, ndmin=2)
         # X coordinate is still wavenumber
-        _z = Coord(
+        _x = Coord(
             reader.xdata, title=reader.xlist_unit.name, units=str(reader.xlist_unit)
         )
         if data.ndim == 2:  # line mapped data
             # y coordinate is the position determined from xpos and ypos
             position = (reader.xpos**2 + reader.ypos**2) ** 0.5
             _y = Coord(position, title="distance", units=str(reader.xpos_unit))
-            dataset.set_coordset(x=_z, y=_y)
+            dataset.set_coordset(x=_x, y=_y)
         elif data.ndim == 3:  # grid mapped data
             grid = reader.map_shape
             xpos = reader.xpos.reshape(grid[::-1])[0]
             ypos = reader.ypos.reshape(grid[::-1])[:, 0]
-            _x = Coord(xpos, title="x position", units=str(reader.xpos_unit))
-            _y = Coord(ypos, title="y position", units=str(reader.ypos_unit))
-            dataset.set_coordset(x=_z, y=_x, z=_y)
+            _w = Coord(xpos, title="x position", units=str(reader.xpos_unit))
+            _h = Coord(ypos, title="y position", units=str(reader.ypos_unit))
+            dataset.set_coordset(x=_x, y=_w, z=_h)
 
         dataset.data = data
         dataset.units = str(reader.spectral_unit)
--- spectrochempy/examples/core/c_importer/plot_read_renishaw.py
+++ spectrochempy/examples/core/c_importer/plot_read_renishaw.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# %%
 # ======================================================================================
 # Copyright (©) 2015-2023 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.
 # CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
@@ -40,20 +41,21 @@ _ = dataset.plot()
 # Now read a series of spectra (measurement type : series) from a Z-depth scan.
 
 dataset = scp.read_wire("ramandata/renishaw/depth.wdf")
-dataset.plot_image()
+_ = dataset.plot_image()
 
 
 # %%
 # filter blank spectra
 keep_rows = np.where(dataset.data.mean(axis=1) > 0)[0]
 dataset = dataset[keep_rows]
-dataset.plot_image()
+_ = dataset.plot_image()
 
 
 # %%
 # extract a line scan data from a StreamLine HR measurement
 dataset = scp.read_wire("ramandata/renishaw/line.wdf")
-dataset.plot_image()
+dataset.y -= dataset.y[0]
+_ = dataset.plot_image()
 
 
 # %%
@@ -61,8 +63,10 @@ dataset.plot_image()
 dataset = scp.read_wire("ramandata/renishaw/mapping.wdf")
 dataset.z -= dataset.z[0]  # this is a xpos
 dataset.y -= dataset.y[0]  # this is a ypos
-dataset.sum(dim=2).plot_image()
-
+# plot the image summming on a frequency
+_ = dataset.sum(dim=2).plot_image()
+# plot the image at 1529cm-1
+_ = dataset[..., 1529.0].plot_image()
 
 # %%
 # This ends the example ! The following line can be uncommented if no plot shows when
