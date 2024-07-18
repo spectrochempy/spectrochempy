cbdf3cc608045892b5b61627dd3f49372e2b6075
atravert <arnaud.travert@ensicaen.fr>
2024-07-18 17:48:55 +0200

    cont

--- tests/test_core/test_readers/test_read_omnic.py
+++ tests/test_core/test_readers/test_read_omnic.py
@@ -86,11 +86,11 @@ def test_read_omnic():
 
     # GC Demo
     a = scp.read_srs("irdata/omnic_series/GC_Demo.srs")
-    assert str(a) == "NDDataset: [float64] V (shape: (y:788, x:1738))"
+    assert str(a) == "NDDataset: [float64] % (shape: (y:788, x:1738))"
 
     # TGA
     a = scp.read_srs("irdata/omnic_series/TGA_Demo.srs")
-    assert str(a) == "NDDataset: [float64] V (shape: (y:485, x:3630))"
+    assert str(a) == "NDDataset: [float64] a.u. (shape: (y:485, x:3630))"
 
     # high speed series
     a = scp.read_srs("irdata/omnic_series/high_speed.srs")
