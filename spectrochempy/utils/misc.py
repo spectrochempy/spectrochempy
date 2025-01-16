a5ba78325637ca020ec6577822b7c502beb2e080
atravert <arnaud.travert@unicaen.fr>
2025-01-16 22:07:26 +0100

    cont

--- spectrochempy/utils/misc.py
+++ spectrochempy/utils/misc.py
@@ -20,10 +20,10 @@ from quaternion import as_float_array, as_quat_array
 #
 # constants
 #
-TYPE_INTEGER = (int, np.int_, np.int32, np.int64, np.uint32, np.uint64)
-TYPE_FLOAT = (float, np.float_, np.float32, np.float64)
-TYPE_COMPLEX = (complex, np.complex_, np.complex64, np.complex128)
-TYPE_BOOL = (bool, np.bool_)
+TYPE_INTEGER = (int, np.int32, np.int64, np.uint32, np.uint64)
+TYPE_FLOAT = (float, np.float16, np.float32, np.float64)
+TYPE_COMPLEX = (complex, np.complex64, np.complex128)
+TYPE_BOOL = (bool, np.bool)
 
 EPSILON = epsilon = np.finfo(float).eps
 "Minimum value before considering it as zero value."
