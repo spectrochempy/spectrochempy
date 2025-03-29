9b56093363a459ca1265a217e97816fb13e83088
Christian Fernandez <fernandez.fabre@gmail.com>
2025-03-29 17:10:06 +0100

    MAINT: clean example for Omnic file reading

--- src/spectrochempy/examples/core/c_importer/plot_read_IR_from_omnic.py
+++ src/spectrochempy/examples/core/c_importer/plot_read_IR_from_omnic.py
@@ -23,11 +23,6 @@ dataset = scp.read_omnic(datadir / "irdata" / "nh4y-activation.spg")
 dataset.plot_stack(style="paper")
 
 # %%
-c = scp.NDDataset
-scp.sort(dataset)
-scp.write(dataset, overwrite=True)
-
-# %%
 # change the unit of y-axis, the y origin as well as the title of the axis
 
 dataset.y.to("hour")
