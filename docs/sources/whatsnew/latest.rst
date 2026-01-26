1586c3cb5939f347e314dafa794bbbee4f0ec9c4
Arnaud TRAVERT <arnaud.travert@unicaen.fr>
2026-01-26 01:22:24 +0100

    remove welcome msg

--- docs/sources/whatsnew/latest.rst
+++ docs/sources/whatsnew/latest.rst
@@ -6,6 +6,11 @@ What's New in Revision 0.8.2.dev
 These are the changes in SpectroChemPy-0.8.2.dev.
 See :ref:`release` for a full changelog, including other versions of SpectroChemPy.
 
+New Features
+~~~~~~~~~~~~
+
+- the welcome message display functionality has been removed
+
 Bug Fixes
 ~~~~~~~~~
 
--- src/spectrochempy/application/start.py
+++ src/spectrochempy/application/start.py
@@ -6,35 +6,6 @@
 """Startup functions for the spectrochempy package."""
 
 
-def display_loading_message(n: int) -> None:
-    """
-    Display a loading message.
-
-    It's a rich text message that is displayed in the Jupyter Notebook environment.
-
-    Parameters
-    ----------
-    n : int
-        Number of dots to display at the end of the message.
-
-    """
-    from spectrochempy.utils.system import is_notebook
-
-    message = f"Loading SpectroChemPy API{'.' * n}"
-    if is_notebook():
-        from IPython.display import clear_output
-        from IPython.display import publish_display_data
-
-        clear_output()
-        info = (
-            f"<div style='color: black; background-color: #f8d7da; padding: 10px; border: 1px solid #f5c6cb; border-radius: 5px;'>"
-            f"{message}</div>"
-        )
-        publish_display_data(data={"text/html": info})
-    else:
-        print(message)  # noqa: T201
-
-
 def set_warnings() -> None:
     """Set warnings for the package."""
     import warnings
