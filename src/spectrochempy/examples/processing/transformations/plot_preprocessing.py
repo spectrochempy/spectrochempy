1476d1b92db07bfadb34ff43eed03558a119c4d1
Christian Fernandez <fernandez.fabre@free.fr>
2026-07-04 08:05:21 +0200

    DOC: Update plot_preprocessing.py
    Zone displayed change

--- src/spectrochempy/examples/processing/transformations/plot_preprocessing.py
+++ src/spectrochempy/examples/processing/transformations/plot_preprocessing.py
@@ -1,3 +1,4 @@
+# %%
 # ======================================================================================
 # Copyright (©) 2014-2026 Laboratoire Catalyse et Spectrochimie (LCS), Caen, France.
 # CeCILL-B FREE SOFTWARE LICENSE AGREEMENT
