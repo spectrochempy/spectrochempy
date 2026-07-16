3f238bd1e741abcb647366f0b5e3953e86ef35d9
Christian Fernandez <fernandez.fabre@gmail.com>
2026-07-16 09:13:30 +0200

    TEST: rewrite and reactivate legacy NMR tests

--- plugins/spectrochempy-nmr/tests/test_nmr_smooth.py
+++ plugins/spectrochempy-nmr/tests/test_nmr_smooth.py
@@ -9,7 +9,6 @@ import numpy as np
 import pytest
 
 import spectrochempy as scp
-from spectrochempy.utils.mplutils import show
 
 DATADIR = scp.preferences.datadir
 NMRDATA = DATADIR / "nmrdata"
@@ -17,15 +16,26 @@ nmrdir = NMRDATA / "bruker" / "tests" / "nmr"
 
 
 @pytest.mark.skipif(not NMRDATA.exists(), reason="NMR test data not available")
-def test_smooth():
+def test_smooth_preserves_shape():
+    path = nmrdir / "topspin_1d" / "1" / "fid"
+    ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
+    dataset = ndd.copy()
+    dataset /= dataset.real.data.max()
+    dataset = dataset.fft(tdeff=8192, size=2**15)
+
+    s = dataset.smooth(size=21)
+    assert s.shape == dataset.shape
+
+
+@pytest.mark.skipif(not NMRDATA.exists(), reason="NMR test data not available")
+def test_smooth_reduces_noise():
     path = nmrdir / "topspin_1d" / "1" / "fid"
     ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
     dataset = ndd.copy()
     dataset /= dataset.real.data.max()
     dataset = dataset.fft(tdeff=8192, size=2**15) + np.random.random(2**15) * 5.0
-    dataset.plot()
 
     s = dataset.smooth(size=21)
-    s.plot(clear=False, color="r", xlim=[20, -20])
-
-    show()
+    assert not np.array_equal(s.data, dataset.data)
+    # smooth should reduce high-frequency noise in quiet regions
+    assert np.std(np.abs(s.data[1000:2000])) < np.std(np.abs(dataset.data[1000:2000]))
--- tests/test_processing/test_fft/test_nmr.py
+++ tests/test_processing/test_fft/test_nmr.py
@@ -8,346 +8,165 @@
 import os
 
 import numpy as np
-import pytest
 
 from spectrochempy.application.preferences import preferences as prefs
-from spectrochempy.core.dataset.nddataset import NDDataset
 from spectrochempy.core.units import ur
-from spectrochempy.utils.mplutils import show
 from spectrochempy.utils.testing import (
     assert_array_almost_equal,
     assert_array_equal,
-    assert_equal,
-)
-
-# TODO(0.9.3): Revisit this legacy coverage after NMR 2D processing is
-# stabilized in the spectrochempy-nmr plugin.
-pytestmark = pytest.mark.skip(
-    "quarantined legacy NMR tests: requires spectrochempy-nmr, NMR test data, "
-    "and NMR 2D review before reactivation"
 )
 
 
-# 1D
-
-# Reader
+# ---------------------------------------------------------------------------
+# 1D reader
+# ---------------------------------------------------------------------------
 
 
 def test_nmr_reader_1D():
+    import spectrochempy as scp
+
     path = os.path.join(
         prefs.datadir, "nmrdata", "bruker", "tests", "nmr", "topspin_1d"
     )
-
-    # load the data in a new dataset
-    import spectrochempy as scp
-
     ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
-    assert ndd.__str__() == "NDDataset: [complex128] unitless (size: 12411)"
-    assert (
-        "<tr><td style='padding-right:5px; padding-bottom:0px; padding-top:0px; width:124px'><font color='#28A745'> "
-        " coordinates</font> </td><td style='text-align:left; padding-bottom:0px; padding-top:0px; border:.5px "
-        "solid lightgray;  '> <div><font color='#2D7FF9'>[       0        4 ... 4.964e+04 4.964e+04] us</font></div>"
-        "</td><tr>" in ndd._repr_html_()
-    )
+    assert ndd.__str__() == "NDDataset: [complex128] count (size: 12411)"
+    assert "coordinates" in ndd._repr_html_()
+    assert ndd.shape == (12411,)
+    assert ndd.data.dtype == np.complex128
 
 
-# plot
+# ---------------------------------------------------------------------------
+# 1D apodization
+# ---------------------------------------------------------------------------
 
 
-def test_nmr_1D_show(NMR_dataset_1D):
-    # test simple plot
-
-    dataset = NMR_dataset_1D.copy()
-    dataset.plot()
-
-    show()
-
-
-def test_nmr_1D_show_complex(NMR_dataset_1D):
-    dataset = NMR_dataset_1D.copy()
-    dataset.plot(xlim=(0.0, 25000.0))
-    dataset.plot(imag=True, color="r", data_only=True, clear=False)
-
-    # display the real and complex at the same time
-    dataset.plot(
-        show_complex=True, color="green", xlim=(0.0, 30000.0), zlim=(-200.0, 200.0)
-    )
-    show()
-
-
-# apodization
-def test_nmr_1D_apodization(NMR_dataset_1D):
+def test_nmr_1D_em(NMR_dataset_1D):
     dataset = NMR_dataset_1D.copy()
     dataset /= dataset.real.data.max()  # normalize
+    original = dataset.data.copy()
 
-    lb = 0
-    arr, apod = dataset.em(lb=lb, retapod=True)
-
-    # arr and dataset should be equal as no em was applied
-    assert_equal(dataset.data, arr.data)
-
-    lb = 0.0
-    gb = 0.0
-    arr, apod = dataset.gm(lb=lb, gb=gb, retapod=True)
-
-    # arr and dataset should be equal as no em was applied
-    assert_equal(dataset.data, arr.data)
-
-    lb = 100
-    arr, apod = dataset.em(lb=lb, retapod=True)
-
-    # arr and dataset should not be equal as inplace=False
-    assert not np.array_equal(dataset.data, arr.data)
+    # em with inplace=False returns a new object with modified data
+    arr, apod = dataset.em(lb=100, retapod=True)
+    assert not np.array_equal(arr.data, original)
+    assert arr.shape == dataset.shape
     assert_array_almost_equal(apod[1], 0.9987, decimal=4)
 
-    # inplace=True
-    dataset.plot(xlim=(0.0, 6000.0))
+    # em with inplace=True modifies the dataset in place
+    dataset2 = NMR_dataset_1D.copy()
+    dataset2 /= dataset2.real.data.max()
+    original2 = dataset2.data.copy()
+    dataset2.em(lb=100.0 * ur.Hz, inplace=True)
+    assert not np.array_equal(dataset2.data, original2)
 
-    dataset.em(lb=100.0 * ur.Hz, inplace=True)
-    dataset.plot(c="r", data_only=True, clear=False)
 
-    # successive call
-    dataset.em(lb=200.0 * ur.Hz, inplace=True)
-    dataset.plot(c="g", data_only=True, clear=False)
-
+def test_nmr_1D_gm(NMR_dataset_1D):
     dataset = NMR_dataset_1D.copy()
-    dataset.plot()
+    dataset /= dataset.real.data.max()
 
-    dataset.em(100.0 * ur.Hz, inplace=True)
-    dataset.plot(c="r", data_only=True, clear=False)
-
-    # first test gm
-    dataset = NMR_dataset_1D.copy()
-    dataset /= dataset.real.data.max()  # normalize
-
-    dataset.plot(xlim=(0.0, 6000.0))
-
-    dataset, apod = dataset.gm(lb=-100.0 * ur.Hz, gb=100.0 * ur.Hz, retapod=True)
-    dataset.plot(c="b", data_only=True, clear=False)
-    apod.plot(c="r", data_only=True, clear=False)
-
-    show()
+    dataset_gm, apod = dataset.gm(lb=-100.0 * ur.Hz, gb=100.0 * ur.Hz, retapod=True)
+    assert dataset_gm.shape == dataset.shape
+    assert not np.array_equal(dataset_gm.data, dataset.data)
+    assert apod.shape == dataset.shape
 
 
-# FFT
+# ---------------------------------------------------------------------------
+# 1D FFT
+# ---------------------------------------------------------------------------
 
 
 def test_nmr_fft_1D(NMR_dataset_1D):
     dataset1D = NMR_dataset_1D.copy()
     dataset1D /= dataset1D.real.data.max()  # normalize
     dataset1D.x.ito("s")
+
+    # FFT produces correct shape and dtype
     new = dataset1D.fft(tdeff=8192, size=2**15)
-    new.plot()
+    assert new.shape == (2**15,)
+    assert new.data.dtype == np.complex128
+    assert np.any(new.data.imag != 0)  # result should be complex
+
+    # IFFT preserves energy (Parseval's theorem)
+    energy_time = np.sum(np.abs(dataset1D.data) ** 2)
     new2 = new.ifft()
-    dataset1D.plot()
-    (new2 - 1.0).plot(color="r", clear=False)
-    show()
+    energy_roundtrip = np.sum(np.abs(new2.data) ** 2)
+    assert abs(energy_roundtrip / energy_time - 1.0) < 0.01
 
 
-def test_nmr_fft_1D_our_Hz(NMR_dataset_1D):
-    dataset1D = NMR_dataset_1D.copy()
-    dataset1D /= dataset1D.real.data.max()  # normalize
-    LB = 10.0 * ur.Hz
-    GB = 50.0 * ur.Hz
-    dataset1D.gm(gb=GB, lb=LB)
-    new = dataset1D.fft(size=32000, ppm=False)
-    new.plot(xlim=[5000, -5000])
-    show()
+# ---------------------------------------------------------------------------
+# 1D manual phasing
+# ---------------------------------------------------------------------------
 
 
 def test_nmr_manual_1D_phasing(NMR_dataset_1D):
     dataset1D = NMR_dataset_1D.copy()
     dataset1D /= dataset1D.real.data.max()  # normalize
 
-    dataset1D.em(10.0 * ur.Hz)  # inplace broadening
-    transf = dataset1D.fft(tdeff=8192, size=2**15)  # fft
-    transf.plot()  # plot)
+    dataset1D.em(lb=10.0 * ur.Hz)
+    transf = dataset1D.fft(tdeff=8192, size=2**15)
 
-    # manual phasing
-    transfph = transf.pk(verbose=True)  # by default pivot = 'auto'
-    transfph.plot(xlim=(20, -20), clear=False, color="r")
-    assert_array_equal(transfph.data, transf.data)  # because phc0 already applied
+    # phasing with default pivot and phc0=0 should return same data
+    transfph = transf.pk(verbose=True)
+    assert_array_equal(transfph.data, transf.data)
 
+    # with phc0=0 (default), pivot position doesn't change the data
     transfph3 = transf.pk(pivot=50, verbose=True)
-    transfph3.plot(clear=False, color="r")
-    not assert_array_equal(
-        transfph3.data, transfph.data
-    )  # because phc0 already applied
-    #
+    assert_array_equal(transfph3.data, transfph.data)
+
+    # phc0 != 0 changes the data
     transfph4 = transf.pk(pivot=100, phc0=40.0, verbose=True)
-    transfph4.plot(xlim=(20, -20), clear=False, color="g")
-    assert transfph4 != transfph
+    assert not np.array_equal(transfph4.data, transf.data)
 
-    transfph4 = transf.pk(pivot=100, verbose=True, inplace=True)
-    (transfph4 - 10).plot(xlim=(20, -20), clear=False, color="r")
-
-    show()
+    # inplace=True returns the same object
+    transfph5 = transf.pk(pivot=100, verbose=True, inplace=True)
+    assert transfph5 is transf
 
 
-def test_nmr_auto_1D_phasing():
-    path = os.path.join(
-        prefs.datadir, "nmrdata", "bruker", "tests", "nmr", "topspin_1d"
-    )
-    ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
-    ndd /= ndd.real.data.max()  # normalize
-    ndd.em(10.0 * ur.Hz, inplace=True)
-    transf = ndd.fft(tdeff=8192, size=2**15)
-    transf.plot(xlim=(20, -20), ls=":", color="k")
-
-    transfph2 = transf.pk(verbose=True)
-    transfph2.plot(xlim=(20, -20), clear=False, color="r")
-
-    # automatic phasing
-    transfph3 = transf.apk(verbose=True)
-    (transfph3 - 1).plot(xlim=(20, -20), clear=False, color="b")
-
-    transfph4 = transf.apk(algorithm="acme", verbose=True)
-    (transfph4 - 2).plot(xlim=(20, -20), clear=False, color="g")
-
-    transfph5 = transf.apk(algorithm="neg_peak", verbose=True)
-    (transfph5 - 3).plot(xlim=(20, -20), clear=False, ls="-", color="r")
-
-    transfph6 = transf.apk(algorithm="neg_area", verbose=True)
-    (transfph6 - 4).plot(xlim=(20, -20), clear=False, ls="-.", color="m")
-
-    transfph4 = transfph6.apk(algorithm="acme", verbose=True)
-    (transfph4 - 6).plot(xlim=(20, -20), clear=False, color="b")
-
-    show()
-
-
-def test_nmr_multiple_manual_1D_phasing():
-    path = os.path.join(
-        prefs.datadir, "nmrdata", "bruker", "tests", "nmr", "topspin_1d"
-    )
-    ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
-    ndd /= ndd.real.data.max()  # normalize
-    ndd.em(10.0 * ur.Hz)  # inplace broadening
-
-    transf = ndd.fft(tdeff=8192, size=2**15)
-
-    transfph1 = transf.pk(verbose=True)
-    transfph1.plot(xlim=(20, -20), color="k")
-
-    transfph2 = transf.pk(verbose=True)
-    transfph2.plot(xlim=(20, -20), clear=False, color="r")
-
-    transfph3 = transf.pk(52.43836, -16.8366, verbose=True)
-    transfph3.plot(xlim=(20, -20), clear=False, color="b")
-
-    show()
-
-
-def test_nmr_multiple_auto_1D_phasing():
-    path = os.path.join(
-        prefs.datadir, "nmrdata", "bruker", "tests", "nmr", "topspin_1d"
-    )
-    ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
-    ndd /= ndd.real.data.max()  # normalize
-    ndd.em(10.0 * ur.Hz)  # inplace broadening
-
-    transf = ndd.fft(tdeff=8192, size=2**15)
-    transf.plot(xlim=(20, -20), ls=":", color="k")
-
-    t1 = transf.apk(algorithm="neg_peak", verbose=True)
-    (t1 - 5.0).plot(xlim=(20, -20), clear=False, color="b")
-
-    t2 = t1.apk(algorithm="neg_area", verbose=True)
-    (t2 - 10).plot(xlim=(20, -20), clear=False, ls="-.", color="m")
-
-    t3 = t2.apk(algorithm="acme", verbose=True)
-    (t3 - 15).plot(xlim=(20, -20), clear=False, color="r")
-
-    show()
-
-
-# #### 2D NMR ########
+# ---------------------------------------------------------------------------
+# 2D reader
+# ---------------------------------------------------------------------------
 
 
 def test_nmr_reader_2D():
+    import spectrochempy as scp
+
     path = os.path.join(
         prefs.datadir, "nmrdata", "bruker", "tests", "nmr", "topspin_2d"
     )
-
-    # load the data in a new dataset
-    import spectrochempy as scp
-
     ndd = scp.read_topspin(path, expno=1, remove_digital_filter=True)
-    assert "unitless" in ndd.__str__()
-    assert "(shape: (y:96, x:948))" in ndd.__str__()
-    assert (
-        "<tr><td style='padding-right:5px; padding-bottom:0px; padding-top:0px;"
-        in ndd._repr_html_()
-    )
+    assert "count" in ndd.__str__()
+    assert "(shape: (y:96, x:474))" in ndd.__str__()
+    assert "coordinates" in ndd._repr_html_()
+    assert ndd.shape == (96, 474)
 
 
-def test_nmr_2D_imag(NMR_dataset_2D):
-    # plt.ion()
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot(imag=True)
-    show()
-
-
-def test_nmr_2D_imag_compare(NMR_dataset_2D):
-    # plt.ion()
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot()
-    dataset.plot(imag=True, cmap="jet", data_only=True, alpha=0.3, clear=False)
-    # better not to replot a second colorbar
-    show()
-
-
-def test_nmr_2D_hold(NMR_dataset_2D):
-    dataset = NMR_dataset_2D
-    dataset.plot()
-    dataset.imag.plot(cmap="jet", data_only=True, clear=False)
-    show()
-
-
-# apodization
+# ---------------------------------------------------------------------------
+# 2D em (axis-specific, shape preservation)
+# ---------------------------------------------------------------------------
 
 
 def test_nmr_2D_em_x(NMR_dataset_2D):
     dataset = NMR_dataset_2D.copy()
-    assert dataset.shape == (96, 948)
-    dataset.plot_map()  # plot original
+    assert dataset.shape == (96, 474)
 
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot_map()
+    # em on F2 axis preserves shape
     dataset.em(lb=50.0 * ur.Hz, axis=-1)
-    assert dataset.shape == (96, 948)
-    dataset.plot_map(cmap="copper", data_only=True, clear=False)  # em on dim=x
+    assert dataset.shape == (96, 474)
 
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot_map()
-    dataset.em(lb=50.0 * ur.Hz, dim="x")
-    assert dataset.shape == (96, 948)
-    dataset.plot_map(cmap="copper", data_only=True, clear=False)  # em on dim=x
-
-    show()
+    # em with dim="x" preserves shape
+    dataset2 = NMR_dataset_2D.copy()
+    dataset2.em(lb=50.0 * ur.Hz, dim="x")
+    assert dataset2.shape == (96, 474)
 
 
 def test_nmr_2D_em_y(NMR_dataset_2D):
     dataset = NMR_dataset_2D.copy()
-    assert dataset.shape == (96, 948)
-    dataset.plot_map()  # plot original
+    assert dataset.shape == (96, 474)
 
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot_map()
+    # em on F1 axis preserves shape
     dataset.em(lb=50.0 * ur.Hz, dim=0)
-    assert dataset.shape == (96, 948)
-    dataset.plot_map(cmap="copper", data_only=True, clear=False)  # em on dim=x
+    assert dataset.shape == (96, 474)
 
-    dataset = NMR_dataset_2D.copy()
-    dataset.plot_map()
-    dataset.em(lb=50.0 * ur.Hz, dim="y")
-    assert dataset.shape == (96, 948)
-    dataset.plot_map(cmap="copper", data_only=True, clear=False)  # em on dim=x
-
-    show()
-
-
-def test_nmr_2D(NMR_dataset_2D):
-    dataset = NMR_dataset_2D
-    dataset.plot(nlevels=20)  # , start=0.15)
-    show()
+    # em with dim="y" preserves shape
+    dataset2 = NMR_dataset_2D.copy()
+    dataset2.em(lb=50.0 * ur.Hz, dim="y")
+    assert dataset2.shape == (96, 474)
